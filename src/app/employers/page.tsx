43e3ba3e3d7a25207dcff6dd2b9a039eecd6f1e5
Yonas <yonas.tefera30@gmail.com>
2026-06-07 20:34:03 -0400

    fix build warning

--- src/app/employers/page.tsx
+++ src/app/employers/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import EmployerLandingPage from "@/employer/landing/EmployerLandingPage";
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: "For Employers | HireGeneral",
   description:
